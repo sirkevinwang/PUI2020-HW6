4e0d88a9b7096643c6ad039c8413de7e1d5ffcc1
Kevin Wang <kevin.wang11@outlook.com>
2020-10-30 06:03:47 -0400

    added some flexboxes

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -23,39 +23,49 @@ const Cart = (props) => {
         </div>
       </div>
       <div id="cart-container">
-        <div>
-          <div>
+        <div className="cart-item">
+          <div className="column">
             <img src={OriginalImage}></img>
           </div>
-          <div>
+          <div className="column middle">
             <h3>Classic</h3>
             <span>$3.99 / pc</span><br></br>
             <span>Vanilla Milk Glazing</span>
           </div>
-          <div>
+          <div className="column side">
             <a>Edit</a>
             <br></br>
             <a>Delete</a>
           </div>
         </div>
-        <table>
-          <tr>
-            <td>Subtotal</td>
-            <td>$3.99</td>
-          </tr>
-          <tr>
-            <td>Shipping</td>
-            <td>FREE</td>
-          </tr>
-          <tr>
-            <td>Estimated tax for 15232</td>
-            <td>$0.28</td>
-          </tr>
-          <tr>
-            <td>Total</td>
-            <td>$4.27</td>
-          </tr>
-        </table>
+        <hr></hr>
+        <div id="receipt">
+          <table className="full-width">
+            <tbody>
+              <tr>
+                <td>Subtotal</td>
+                <td>$3.99</td>
+              </tr>
+              <tr>
+                <td>Shipping</td>
+                <td>FREE</td>
+              </tr>
+              <tr>
+                <td>Estimated tax for 15232</td>
+                <td>$0.28</td>
+              </tr>
+              <hr></hr>
+              <tr>
+                <td>Total</td>
+                <td>$4.27</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="add-to-cart-btns my-4">
+          <span className="round-btn mr-1">Check Out</span>
+          <span className="round-btn">Buy with &#xF8FF;Pay</span>
+        </div>
       </div>
     </div>
 
